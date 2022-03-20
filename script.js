75034884fd193806ef6f67fc73dc2431b77253aa
HM-711 <81849211+HM-711@users.noreply.github.com>
2022-03-20 21:10:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,45 @@
 /* Fill your code*/
-document.getElementById('addBlog').addEventListener('click',function()
-{
-    document.getElementById('popupContact').style.display = 'block';
+document.getElementById('addBlog').addEventListener('click',function(){
+document.getElementById('popupContact').style.display="block";
+
+
 })
-class Blog
-{
-    constructor(title,detail)
-    {
-        this.title = title
-        this.detail = detail
+document.getElementById('close').addEventListener('click',function(){
+    document.getElementById('popupContact').style.display="none";
+})
+class Blog{
+    constuctor(title,detail){
+        this.title=title
+        this.detail=detail
     }
-
-    addTitle()
-    {
-        var title_card = document.createElement('h1');
-        title_card.setAttribute("id","blog-title");
-        console.log(title_card);
-        document.getElementById('card-text').appendChild(title_card);
-        title_card.innerHTML += this.title;
-    }
-
-    addDescription()
-    {
-        var display = document.createElement('p');
-        display.setAttribute("id","blog-detail");
-        console.log(display);
-        document.getElementById('card-text').appendChild(display);
-        display.innerHTML += this.detail;
-    }
+    addTitle(){
+    var title_card = document.createElement('h1');
+    title_card.setAttribute("id","blog-title");
+    console.log(title_card);
+    document.getElementById('card-text').appendChild(title_card);
+    title_card.innerHTML += this.title;
+    
+    
+   
+     } 
+     addDescription(){
+       var display_card = document.createElement('p');
+        display_card.setAttribute("id","blog-description");
+        console.log(display_card);
+        document.getElementById('card-text').appendChild(display_card);
+        display_card.innerHTML += this.detail ;
+     }
 }
+var post=document.getElementById('post');
+post.addEventListener('click',function(){
+    var image=document.createElement('img')
+ image.src='./assets/javascript.png';
+image.setAttribute('id','image')
+document.getElementById('card-text').appendChild(image);
+var title=document.getElementById('title').value;
+var detail=document.getElementById('detail').value;
+var card=new Blog(title,detail)
+card.addTitle()
+card.addDescription()
 
-var title = document.getElementById('title').value;
-var detail = document.getElementById('detail').value;
-if(title.length != 0 || detail.length != 0)
-{
-    var post = document.getElementById('post');
-    post.addEventListener('click',function()
-    {
-        var image = document.createElement('img');
-        image.src ="./assets/javascript.png";
-        document.getElementById("flashcard").append(image);
-        var add_blog = new Blog(title,detail);
-        add_blog.addTitle();
-        add_blog.addDescription()
-    })
+})
