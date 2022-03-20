82e471266d935b383aa83fcab34e441e6525e480
HM-711 <81849211+HM-711@users.noreply.github.com>
2022-03-20 21:19:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,44 @@
-document.getElementById('addBlog').addEventListener('click', function () {
-    document.getElementById('popupContact').style.display = "block";
-})
-class Blog {
-    constructor(title, detail) {
-        this.title = title;
-        this.detail = detail;
-    }
-    addTitle() {
+class Blog{
+    constuctor(title,detail){
+        this.title = title
+        this.detail = detail
+    }  
+    addTitle(){
         var title_card = document.createElement('h1');
-        title_card.setAttribute("id", "blog-title");
+        title_card.setAttribute("id","blog-title");
         console.log(title_card);
         document.getElementById('card-text').appendChild(title_card);
         title_card.innerHTML += this.title;
-
-    }
-    addDescription() {
-        var discription_card = document.createElement('p');
-        discription_card.setAttribute("id", "blog-description");
-        console.log(discription_card);
-        document.getElementById('card-text').appendChild(discription_card);
-        discription_card.innerHTML += this.detail;
+    }  
+    addDescription(){
+        var detail_card = document.createElement('p');
+        detail_card.setAttribute("id","blog-description");
+        console.log(detail_card);
+        document.getElementById('card-text').appendChild(detail_card);
+        detail_card.innerHTML += this.detail;
     }
 }
 
-var post = document.getElementById('post');
-post.addEventListener('click', function () {
-    var title = document.getElementById('title').value;
-    var detail = document.getElementById('detail').value;
-    var add_blog = new Blog(title, detail);
-    add_blog.addTitle();
-    add_blog.addDescription();
-    var image = document.createElement('img');
-    image.src = './assets/javascript.png';
-    var newchild = document.getElementById('flashcard')
-    newchild.insertBefore(image, newchild.childNodes[0]);
-    document.getElementById('popupContact').style.display = "none";
-})
+
+document.getElementById("addBlog").addEventListener("click", addPost)
+document.getElementById("close").addEventListener("click", close)
+document.getElementById("post").addEventListener("click", addBlog)
+
+function addPost(){
+    var unhide = document.getElementById("popupContact")
+    unhide.style.display = "flex"
+}
+function close(){
+    var unhide = document.getElementById("popupContact")
+    unhide.style.display = "none"
+}
+function addBlog(){
+    const obj = new Blog
+    obj.title = document.getElementById("title").value
+    obj.detail = document.getElementById("detail").value
+    if((obj.title != "")&&(obj.detail!="")){
+        obj.addTitle()
+        obj.addDescription()
+    }
+    else alert("TITLE OR DESCRIPTION CANNOT BE EMPTY")
+}
