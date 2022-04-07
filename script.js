d3645d09710b8b506168dbb1e452a7d9b012cc31
sachinsh01 <89263668+sachinsh01@users.noreply.github.com>
2022-04-07 23:56:25 +0530

    ProGrad ID : 8731

--- script.js
+++ script.js
@@ -38,4 +38,4 @@ document.getElementById("post").addEventListener("click",function(){
     var $title=document.getElementById("title").value;
     var $detail=document.getElementById("detail").value;
     var object1=new Blog($title,$detail);
-});
+}); 
